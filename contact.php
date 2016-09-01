6da36964a6ea98bd01a9acfe1220be31aa504628
Mark Evangelista <mark172@me.com>
2016-09-01 14:30:27 +0000

    added contact page

---
+++ contact.php
@@ -0,0 +1,44 @@
+
+<?php $page = 'contact' ?>
+<?php include 'header.php' ?>
+
+<!-- Kineoworks Contact -->
+    <div class="kineoworks-contact">
+      <div class="container">
+        <div class="row">
+
+          <!-- Nav tabs -->
+
+          
+          <!-- Tab panes -->
+          <div class="tab-content">
+            <div class="" id="question">
+              <h2 class="h2-responsive">Questions?<br><small>Bring them on!</small></h2>
+              <form class="form-horizontal" role="form">
+                <div class="form-group">
+                  <div class="col-sm-6">
+                    <input type="text" class="form-control" id="inputNameQst" placeholder="Your name">
+                  </div>
+                  <div class="col-sm-6">
+                    <input type="email" class="form-control" id="inputEmailQst" placeholder="Your email">
+                  </div>  
+                </div>
+                <div class="form-group">
+                  <div class="col-sm-12">
+                    <textarea class="form-control" rows="6" placeholder="Your message"></textarea>
+                  </div>
+                </div> 
+                <div class="form-group">
+                  <div class="col-sm-12">
+                    <button type="button" class="btn btn-testimonial btn-lg btn-block">Submit Question</button>
+                  </div>
+                </div>  
+              </form>
+            </div>
+
+          </div> <!-- /.tab-content -->
+        </div> <!-- /.row -->  
+      </div> <!-- /.container --> 
+    </div> <!-- /.kineoworks-contact -->
+    
+<?php include 'footer.php' ?>
